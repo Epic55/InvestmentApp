db00f6a5466be606f517ded5835fd00a10750e10
Epic55 <alibek555@gmail.com>
2022-03-03 12:15:20 +0600

    v2

--- main.py
+++ main.py
@@ -11,3 +11,5 @@ for i in iter(int,1):
     print("\n","Choose number what u want to do: ")
     print("1 - show all shares, 2 - insert data about new share, 3 - delete data about share, 4 - update data about share, 5 - exit from programm")
     f(input("Enter number: "))
+#test case
+f(1)
