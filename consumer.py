5b053112981b0a3e5da031387c4664ffee68c215
Epic55 <alibek555@gmail.com>
2022-03-06 12:17:37 +0600

    Create rabbitmq files

---
+++ consumer.py
@@ -0,0 +1,25 @@
+import pika, os, time
+
+def process_function(msg):
+  print(" Msg processing ...")
+  print(" [x] Received " + str(msg))
+
+  time.sleep(5) # delays for 5 seconds
+  print(" PDF processing finished");
+  return;
+
+url = os.environ.get('CLOUDAMQP_URL', 'amqps://qcrhjtsz:4bATF2MDI0JTgtZaGrs6GiRlFoYKcHyd@puffin.rmq2.cloudamqp.com/qcrhjtsz')
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='MyQueue1')
+
+# create a function which is called on incoming messages
+def callback(ch, method, properties, body):
+  process_function(body)
+
+# set up subscription on the queue
+channel.basic_consume('MyQueue1', callback, auto_ack=True)
+
+channel.start_consuming()
+connection.close()
