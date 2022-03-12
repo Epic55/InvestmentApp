811cb709284df973b8d001adcfd27137271ad8d4
Epic55 <alibek555@gmail.com>
2022-03-12 21:39:29 +0600

    updated api file

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ def getapi():
     querystring = {"q": input("Enter company "), "region": "US" }
     headers = {
         'x-rapidapi-host': "yh-finance.p.rapidapi.com",
-        'x-rapidapi-key': "fe8d60d5eemshf389a807c4b6cb7p1e02ffjsn2091996495cc"
+        'x-rapidapi-key': ""
         }
     response = requests.request("GET", url, headers=headers, params=querystring)
     print(response.text)
