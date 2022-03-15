bb0f6ad22cf71901c9534fb731a899a5a6be300c
Epic55 <38029225+Epic55@users.noreply.github.com>
2022-03-15 17:38:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ for i in iter(int,1):
     print("\n","Choose number what u want to do: ")
     print(" 1 - show all shares, 2 - insert data about new share, 3 - delete data about share,","\n",
           "4 - update data about share, 5 - send msg via rabbitmq, 6 - launch GUI,","\n",
-          "7 - use api to get info about company, 8 - exit")
+          "7 - call api to get real price of share, 8 - exit")
     f(input("Enter number: "))
